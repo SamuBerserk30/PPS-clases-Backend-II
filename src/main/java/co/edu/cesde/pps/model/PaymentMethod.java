c6e0a235a2c5e613e2220b150c17386e97959e6e
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-14 21:19:06 -0500

    Anotaciones de bombok en PaymentMethod

--- src/main/java/co/edu/cesde/pps/model/PaymentMethod.java
+++ src/main/java/co/edu/cesde/pps/model/PaymentMethod.java
@@ -1,5 +1,7 @@
 package co.edu.cesde.pps.model;
 
+import lombok.*;
+
 import java.util.Objects;
 
 /**
@@ -14,14 +16,18 @@ import java.util.Objects;
  * Relaciones (futuro - etapa02):
  * - 1:N con Payment (un método puede usarse en múltiples pagos)
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class PaymentMethod {
 
     private Long paymentMethodId;
     private String name;
 
     // Constructor vacío (requerido para JPA futuro)
-    public PaymentMethod() {
-    }
 
     // Constructor con campos obligatorios
     public PaymentMethod(String name) {
@@ -29,22 +35,6 @@ public class PaymentMethod {
     }
 
     // Getters y Setters
-
-    public Long getPaymentMethodId() {
-        return paymentMethodId;
-    }
-
-    public void setPaymentMethodId(Long paymentMethodId) {
-        this.paymentMethodId = paymentMethodId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     // equals y hashCode basados en ID
 
@@ -63,11 +53,4 @@ public class PaymentMethod {
 
     // toString sin navegación a objetos relacionados
 
-    @Override
-    public String toString() {
-        return "PaymentMethod{" +
-                "paymentMethodId=" + paymentMethodId +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
