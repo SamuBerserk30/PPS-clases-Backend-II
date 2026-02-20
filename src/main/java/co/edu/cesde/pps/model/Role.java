0cf8566e7222da3453dc929217fd3a510d566f61
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-19 20:58:10 -0500

    poniendo anotaciones jpa en Role

--- src/main/java/co/edu/cesde/pps/model/Role.java
+++ src/main/java/co/edu/cesde/pps/model/Role.java
@@ -1,5 +1,6 @@
 package co.edu.cesde.pps.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Objects;
@@ -17,6 +18,8 @@ import java.util.Objects;
  * Relaciones (futuro - etapa02):
  * - 1:N con User (un rol puede tener múltiples usuarios)
  */
+@Entity
+@Table(name = "role")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -25,8 +28,15 @@ import java.util.Objects;
 
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long roleId;
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name;
+
+    @Column(name = "description", length = 255)
     private String description;
 
     // Constructor vacío (requerido para JPA futuro)
