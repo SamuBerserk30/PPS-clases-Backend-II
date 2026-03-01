dadf06bfff795ad57438d74763c53911010be7af
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-28 22:33:27 -0500

    Anñadiendo anotaciones JPA en OrderItem

--- src/main/java/co/edu/cesde/pps/model/OrderItem.java
+++ src/main/java/co/edu/cesde/pps/model/OrderItem.java
@@ -2,6 +2,7 @@ package co.edu.cesde.pps.model;
 
 import co.edu.cesde.pps.util.CalculationUtils;
 import co.edu.cesde.pps.util.ValidationUtils;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -38,6 +39,8 @@ import java.util.Objects;
  * - N:1 con Order (muchos items pertenecen a una orden)
  * - N:1 con Product (muchos items referencian a un producto)
  */
+@Entity
+@Table(name = "order_items")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -46,11 +49,24 @@ import java.util.Objects;
 
 public class OrderItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id", nullable = false)
     private Long orderItemId;
+
+    @Column(name = "order_id", nullable = false, length = 50)
     private Order order;
+
+    @Column(name = "product_id", nullable = false, length = 50)
     private Product product;
+
+    @Column(name = "quantity", nullable = false, length = 50)
     private Integer quantity;
+
+    @Column(name = "unit_price", nullable = false, length = 50)
     private BigDecimal unitPrice;
+
+    @Column(name = "line_total", nullable = false, length = 50)
     private BigDecimal lineTotal;
 
     // Constructor vacío (requerido para JPA futuro)
