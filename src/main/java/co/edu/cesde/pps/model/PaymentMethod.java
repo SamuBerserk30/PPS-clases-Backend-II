563ae4f2488c7bbe9d6e2d87f0141b6196ec7118
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-28 23:22:44 -0500

    Anñadiendo anotaciones JPA en PaymentMethod

--- src/main/java/co/edu/cesde/pps/model/PaymentMethod.java
+++ src/main/java/co/edu/cesde/pps/model/PaymentMethod.java
@@ -1,5 +1,6 @@
 package co.edu.cesde.pps.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Objects;
@@ -16,6 +17,8 @@ import java.util.Objects;
  * Relaciones (futuro - etapa02):
  * - 1:N con Payment (un método puede usarse en múltiples pagos)
  */
+@Entity
+@Table(name = "payment_methods")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,7 +27,12 @@ import java.util.Objects;
 
 public class PaymentMethod {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_method_id")
     private Long paymentMethodId;
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name;
 
     // Constructor vacío (requerido para JPA futuro)
