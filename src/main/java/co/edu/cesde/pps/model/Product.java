1e38bfd401eb93377d64311ae5bbc34fcff0c8c3
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-03-11 21:04:02 -0500

    Agregando relaciones entre tablas en Product

--- src/main/java/co/edu/cesde/pps/model/Product.java
+++ src/main/java/co/edu/cesde/pps/model/Product.java
@@ -47,7 +47,8 @@ public class Product {
     @Column(name = "product_id", nullable = false)
     private Long productId;
 
-    @Column(name = "category_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
     @Column(name = "sku", nullable = false, unique = true, length = 50)
