27087da24f873b3164f9094845fd1079522f72b9
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-03-28 20:24:11 -0500

    Remplazando en addresRepository

--- src/main/java/co/edu/cesde/pps/repository/AddressRepository.java
+++ src/main/java/co/edu/cesde/pps/repository/AddressRepository.java
@@ -3,5 +3,10 @@ package co.edu.cesde.pps.repository;
 import co.edu.cesde.pps.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    long countByUser_UserId(Long userId);
+
+    List<Address> findByUser_UserId(Long userId);
 }
